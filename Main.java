882e87070043d687de81bd3da01b1acfd8992fc7
RenderNNNN <111516134+RenderNNNN@users.noreply.github.com>
2022-09-13 23:08:37 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -14,57 +14,56 @@ public class Main{
          */
 
 
-        Pokemon BULBASAUR = new Pokemon("Bulbasaur", 45, 45, 49,
-                65, 49, 65, 6.9, 10, new int[]{3, 7});
-        //Attacks
-        Attack Double_Edge = new Attack("Double-Edge", 120, 100, 6.5, -1, 0);
-        Double_Edge.setEffect(100, new Recoil(33, Double_Edge));
-        Attack Leech_Seed = new Attack("Leech Seed", 90, new Poison(8.0, 1, false, true), 3);
-        Attack Sleep_Powder = new Attack("Sleep Powder", 75, new Incapacitate("sleep"), 3);
-
-
 
         Pokemon CHARMANDER = new Pokemon("Charmander", 39, 45, 52,
                 60, 43, 50, 8.5, 10, new int[]{1});
-        //Attacks
-        Attack Ember = new Attack("Ember", 40, 100, 6.5, -1, 1);
-        Ember.setEffect(10, new Poison(16.0, 0.5, false, false));
-        Attack Smokescreen = new Attack("Smokescreen", 100, new ChangeStage("accuracy", -1), 0);
+        //Attacks: Ember, Smokescreen, Dragon_Breath, Slash
+        Attack Ember = new Attack("Ember", 40, 100, 6.5, 0, 1);
+        Ember.addEffect(10, new Damage("burn"));
+        Attack Smokescreen = new Attack("Smokescreen", 0, 100, 0, 0, 3);
+        Smokescreen.addEffect(100, new ChangeStage("accuracy", -1));
+        Attack Dragon_Breath = new Attack("Dragon Breath", 60, 100, 6.5, 0, 14);
+        Dragon_Breath.addEffect(30, new Incapacitate("paralyze"));
+        Attack Slash = new Attack("Slash", 70, 100, 13, 0, 0);
+        CHARMANDER.attacks = new Attack[]{Ember, Smokescreen, Dragon_Breath, Slash};
 
 
 
-
+        Pokemon SQUIRTLE = new Pokemon("Squirtle", 44, 43, 48,
+                50, 65, 64, 9, 10, new int[]{2});
+        //Attacks: Water_Gun, Withdraw, Water Pulse, Protect
+        Attack Water_Gun = new Attack("Water Gun", 40, 100, 6.5, 0, 2);
+        Attack Withdraw = new Attack("Withdraw", 0, 100, 0, 0, 2);
+        Withdraw.addEffect(100, new ChangeStage("defense", 1));
+        Attack Water_Pulse = new Attack("Water Pulse", 60, 100, 6.5, 0, 2);
+        Water_Pulse.addEffect(20, new Confusion(100));
+        Attack Protect = new Attack("Protect", 0, 100, 0, 4, 0);
+        Protect.addEffect(100, new Protection()); //Note: Protection acts like an offensive attack
+        SQUIRTLE.attacks = new Attack[]{Water_Gun, Withdraw, Water_Pulse, Protect};
 
 
 
         //Testing
-        System.out.println(BULBASAUR.getAccuracy());
-        Smokescreen.use(CHARMANDER, BULBASAUR);
-        System.out.println(BULBASAUR.getAccuracy());
+        System.out.println("Before:");
+        System.out.println("Squirtle health: " + SQUIRTLE.getHp());
+        System.out.println("Charmander health: " + CHARMANDER.getHp());
 
+        Protect.use(SQUIRTLE, CHARMANDER);
+        Slash.use(CHARMANDER, SQUIRTLE);
 
         //Simulating passage of time
-        BULBASAUR.update();
+        SQUIRTLE.update();
         CHARMANDER.update();
         for(String message : data) System.out.println(message);
         turn++;
         data = new ArrayList<>();
-        BULBASAUR.update();
+        SQUIRTLE.update();
         CHARMANDER.update();
         for(String message : data) System.out.println(message);
-        System.out.println(CHARMANDER.getHealth());
-        System.out.println(BULBASAUR.getHealth());
+
+        System.out.println("\nAfter:");
+        System.out.println("Squirtle health: " + SQUIRTLE.getHp());
+        System.out.println("Charmander health: " + CHARMANDER.getHp());
         //Printing data
-
     }
-
-    public static void initializePokemon(){
-
-    }
-
-
-
-
-
-
 }
