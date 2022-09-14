f060104ada9af666ad41b7903bb580b4f1c4a3fb
RenderNNNN <111516134+RenderNNNN@users.noreply.github.com>
2022-09-13 23:03:33 -0500

    Update Attack.java

--- Attack.java
+++ Attack.java
@@ -1,25 +1,24 @@
 import java.util.ArrayList;
 public class Attack{
     public static final double[][] typeChart = getTypeChart();
-    public Pokemon user;
     public String name;
     public double power;
     public double accuracy;
-    public double effectAccuracy;
     public double chanceOfCritical;
     public int priority;
     public int type;
 
     public boolean special;
     public boolean status;
-    public Effect effect;
+    public boolean useEffects = true;
+    public ArrayList<Effect> effectList = new ArrayList<>();
+    public ArrayList<Integer> effectAccList = new ArrayList<>();
+    //public double effectAccuracy;
     public double damage = 0;
     public String result = "";
 
     //Normal attack constructor
-    public Attack(String name, double power, double accuracy,
-                  double chanceOfCritical, int priority, int type){
-        //this.user = user;
+    public Attack(String name, double power, double accuracy, double chanceOfCritical, int priority, int type){
         this.name = name;
         this.power = power;
         this.accuracy = accuracy;
@@ -30,33 +29,23 @@ public class Attack{
                 || type == 5 || type == 10 || type == 14 || type == 15);
         this.status = false;
     }
-    //Status attack constructor
-    public Attack(String name, double effectAccuracy, Effect effect, int type){
-        //this.user = user;
-        this.name = name;
-        this.accuracy = 100;
-        this.effectAccuracy = effectAccuracy;
-        this.effect = effect;
-        this.type = type;
-        this.special = (type == 1 || type == 2 || type == 3 || type == 4
-                || type == 5 || type == 10 || type == 14 || type == 15);
-        this.status = true;
+
+    public void addEffect(int effectAcc, Effect effect){
+        this.effectAccList.add(effectAcc);
+        this.effectList.add(effect);
     }
-    public void setEffect(double effectAccuracy, Effect effect){
-        this.effectAccuracy = effectAccuracy;
-        this.effect = effect;
-    }
+
     //Sets damage
     public void initialize(Pokemon user, Pokemon opponent){
         //Initializing damage modifiers
-        accuracy *= user.getAccuracy();
+        accuracy *= user.getAcc();
         int critical = 1;
         if(Math.random() * 100 < chanceOfCritical) critical = 2;
-        double A = user.attack[1];
-        double D = opponent.defense[1];
+        double A = user.getAtk();
+        double D = opponent.getDef();
         if(special){
-            A = user.specialAttack[1];
-            D = opponent.specialDefense[1];
+            A = user.getSpAtk();
+            D = opponent.getSpDef();
         }
         double STAB = 1;
         for(int opponentType : opponent.type){
@@ -72,46 +61,45 @@ public class Attack{
 
         //Calculating damage (with gen 1 formula)
         damage = ((((((2 * user.level * critical) / 5.0) + 2) * power * (A / D)) / 50) + 2) * STAB * typeMultiplier * random;
-        damage *= user.attackDamageMultiplier;
+        damage *= user.atkDmgMult;
+        if(power == 0) damage = 0;
 
         //Creating result
-        int percent = (int) ((damage * 100) / opponent.health[0]);
-        String result = user.name + " used " + name + "!";
+        int percent = (int) ((damage * 100) / opponent.getBaseHp());
+        /*String result = user.name + " used " + name + "!";
         String effectiveness = "But it failed!";
         if(typeMultiplier == 0.5) effectiveness = "It's not very effective...";
         if(typeMultiplier == 1) effectiveness = "It's effective!";
         if(typeMultiplier >= 2) effectiveness = "It's super effective!\n(" + opponent.name + " lost " + percent + "% of its health!)";
         if(critical == 2) effectiveness = effectiveness + "\nA critical hit!";
-        result = result + "\n" + effectiveness;
-        this.result = result;
+        result = result + "\n" + effectiveness;*/
+        if(damage != 0){ result = opponent.name + " lost " + percent + "% of their health!"; this.result = result; }
     }
 
     //Uses attack
     public void use(Pokemon user, Pokemon opponent){
         //Initializing damage
+        if(user.confused && Math.random() * 100 < 50) opponent = user;
         if(!status) initialize(user, opponent);
 
-        if(Math.random() * 100 < accuracy){ //This is checking to see if the attack was missed
+        if(Math.random() * 100 < accuracy){ //Checking to see if the attack was missed
             Main.data.add(result);
-            opponent.health[1] -= damage;
 
-            //Initializing effects
-            if(effect instanceof Recoil) effect.activate(user, user);
-            else{
-                effect.activate(user, opponent);
-                //System.out.println(user.getName());
+            opponent.setHp(opponent.getHp() - damage);
+
+            //Initializing/Using effects
+            for(int i = 0; i < effectList.size(); i++){
+                Effect effect = effectList.get(i);
+                if(Math.random() * 100 < effectAccList.get(i)){ //Checking to see if the effect was missed
+                    if(effect instanceof Recoil){ effect.activate(user, user); }
+                    else{ effect.activate(user, opponent); }
+                }
             }
         }
         else Main.data.add(this.name + " missed");
     }
 
     private static double[][] getTypeChart(){
-        //0-Normal 1-fire 2-water 3-grass 4-electric 5-ice
-        //6-fighting 7-poison 8-ground 9-flying 10-psychic
-        //11-bug 12-rock 13-ghost 14-dragon 15-dark
-        //16-steel 17-fairy
-        //Special: water, fire, grass, electric, psychic, ice, dragon, dark
-        //1,2,3,4,10,5,14,15
         double[][] list = new double[18][18];
         list[0]  = new double[]{1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.5, 0.0, 1.0, 1.0, 0.5, 1.0};
         list[1]  = new double[]{1.0, 0.5, 0.5, 2.0, 1.0, 2.0, 1.0, 1.0, 1.0, 1.0, 1.0, 2.0, 0.5, 1.0, 0.5, 1.0, 2.0, 1.0};
@@ -133,6 +121,4 @@ public class Attack{
         list[17] = new double[]{1.0, 0.5, 1.0, 1.0, 1.0, 1.0, 2.0, 0.5, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 2.0, 2.0, 0.5, 1.0};
         return list;
     }
-
-
 }
