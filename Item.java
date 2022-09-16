583afc2e6067ce4bbd2218eba8ce7425e78c807a
Shark128 <69443118+Shark128@users.noreply.github.com>
2022-09-15 22:48:50 -0500

    Add files via upload

---
+++ Item.java
@@ -0,0 +1,12 @@
+public class Item {
+    public String name;
+    public Effect effect;
+
+    public Item(String name, Effect effect) {
+        this.name = name;
+        this.effect = effect;
+    }
+    public void use () {
+
+    }
+}
