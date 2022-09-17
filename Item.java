1d403c50d692ac1f2a179e84653e1c72a335d1f4
RenderNNNN <111516134+RenderNNNN@users.noreply.github.com>
2022-09-16 22:40:03 -0500

    Update Item.java

--- Item.java
+++ Item.java
@@ -1,42 +1,40 @@
 import java.util.ArrayList;
-
-public class Item {
+public class Item{
+    public static final ArrayList<Item> itemdex = getItemdex();
     public String name;
     public Effect effect;
     public String desc;
 
-    public Item(String name, Effect effect, String desc) {
+    public Item(String name, Effect effect, String desc){
         this.name = name;
         this.effect = effect;
         this.desc = desc;
     }
-    public void usePokeball (Player user, Player opp) {
-        // the pokeball for capturing is separate from all the other effects
-
-//             40% of the pokemon switching sides
-//            If there is 6 pokepeople on the team then the captured pokemon will disappear, if less than it will join the army
+    public void usePokeball(Player user, Player target){
+        /*
+        The pokeball for capturing is separate from all the other effects
+        40% of the pokemon switching sides
+        If there is 6 pokepeople on the team then the captured pokemon will disappear,
+        if less than it will join the army
+        */
         boolean chance = Math.floor((Math.random() * 100)) < 40;
-        if (user.team.size() == 6) {
-            if (chance) {
-                opp.team.remove(opp.currentPokemon);
-                // use the method for replacement if it is made or make one myself if not
+        if(chance){
+            Pokemon captured = target.currentPokemon;
+            Main.addData(user.getName() + " successfully captured " + captured.getName() + "!");
+            target.getTeam().remove(captured);
+            if(user.getTeam().size() < 6){ user.getTeam().add(captured); }
+            if(target.getTeam().size() == 0){
+                Main.addData(user.getName() + " won!");
+                Main.gameOver = true;
             }
         }
-        else {
-            if (chance) {
-                Pokemon captured = opp.currentPokemon;
-                opp.team.remove(captured);
-                user.team.add(captured);
-            }
-        }
+        else{ Main.addData(user.getName() + "'s poke ball failed to capture " + target.currentPokemon.getName() + "!"); }
     }
-
-    public void use (Pokemon user, Pokemon target) {
-        effect.activate(user, target);
-    }
-
-    public void itemDex () {
-        ArrayList<Item> itemdex = new ArrayList<Item>();
+    
+    public void use(Pokemon user, Pokemon target){ effect.activate(user, target); }
+    
+    public static ArrayList<Item> getItemdex(){
+        ArrayList<Item> itemdex = new ArrayList<>();
         Item Poke_Ball = new Item("Poke Ball", new Effect(), "A device for catching wild Pokemon.");
         // there is no effect because Pokeball is special
         Item Confunder = new Item("Confunder", new Confusion(3), "Confuses the opponent for three turns.");
@@ -52,7 +50,6 @@ public class Item {
         itemdex.add(Shock_Pen);
         itemdex.add(Scope);
         itemdex.add(Force_Field);
+        return itemdex;
     }
-
-
 }
